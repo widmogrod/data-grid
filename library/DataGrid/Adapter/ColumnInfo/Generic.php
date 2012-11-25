43e3021b5c33863b949b6e510c8d991bafc26ae2
Gabriel Habryn <gabriel.habryn@schibsted.pl>
2012-11-25 15:23:55 +0100

    update event behavior

---
+++ library/DataGrid/Adapter/ColumnInfo/Generic.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gabrielhabryn
+ * Date: 25.11.2012
+ * Time: 11:13
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ library/DataGrid/Event/Result/Standard.php
@@ -0,0 +1,68 @@
+<?php
+namespace DataGrid\Event\Result;
+
+class Standard implements ResultInterface
+{
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        // TODO: Implement current() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        // TODO: Implement next() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        // TODO: Implement key() method.
+    }
+
+    public function first()
+    {
+        // TODO: Implement first() method.
+    }
+
+    public function last()
+    {
+        // TODO: Implement last() method.
+    }
+
+    public function append($value)
+    {
+        // TODO: Implement append() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        // TODO: Implement count() method.
+    }
+
+}
